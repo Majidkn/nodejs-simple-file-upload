07ca424cac916ec58f58f03f54913c6a5e0b52c1
Majid Nayyeri <majidrobo@gmail.com>
2017-10-19 19:55:57 +0330

    node.js simple multiple file upload

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var multer  = require('multer');
+var fs  = require('fs');
+
+var app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        var dir = './uploads';
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        }
+        callback(null, dir);
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+var upload = multer({storage: storage}).array('talarPhoto', 12);
+app.post('/upload', function (req, res, next) {
+    upload(req, res, function (err) {
+        if (err) {
+            return res.end("Something went wrong:(");
+        }
+        res.end("Upload completed.");
+    });
+})
+
+app.listen(3000);
