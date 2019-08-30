1b2814be29e70e90b8a196311e34fe918b95193c
Przemyslaw Wrzeszcz <przemyslaw.wrzeszcz@invia.pl>
2019-08-30 13:06:10 +0200

    Add uploaded files list page, server IPs list to index page, formatted page for upload success/failure, updated .gitignore

--- server.js
+++ server.js
@@ -1,34 +1,100 @@
-var express = require('express');
-var multer  = require('multer');
-var fs  = require('fs');
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+const open = require('open');
 
-var app = express();
-app.set('view engine', 'ejs');
+const applicationPort = 3000;
+const uploadDirectory = './uploads';
 
-app.get('/', (req, res) => {
-    res.render('index');
+const ifaces = os.networkInterfaces();
+const allOSIps = [];
+
+Object.keys(ifaces).forEach(function (ifname) {
+    let alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+        }
+
+        if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+            allOSIps.push({
+                name: `${ifname}; ${alias}`,
+                address: iface.address,
+            });
+        } else {
+            // this interface has only one ipv4 adress
+            allOSIps.push({
+                name: ifname,
+                address: iface.address,
+            });
+        }
+
+        ++alias;
+    });
 });
 
-var storage = multer.diskStorage({
+
+if (!fs.existsSync(uploadDirectory)){
+    fs.mkdirSync(uploadDirectory);
+}
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use('/static', express.static(path.join(__dirname, uploadDirectory)));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        allOSIps,
+        applicationPort,
+    });
+});
+
+const storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        var dir = './uploads';
-        if (!fs.existsSync(dir)){
-            fs.mkdirSync(dir);
-        }
-        callback(null, dir);
+        callback(null, uploadDirectory);
     },
     filename: function (req, file, callback) {
         callback(null, file.originalname);
     }
 });
-var upload = multer({storage: storage}).array('files', 12);
+const upload = multer({storage: storage}).array('files', 12);
+
 app.post('/upload', function (req, res, next) {
     upload(req, res, function (err) {
         if (err) {
-            return res.end("Something went wrong:(");
-        }
-        res.end("Upload completed.");
-    });
-})
+            console.error(err);
 
-app.listen(3000);
+            return res.render('uploaded', {
+                message: "Something went wrong :(",
+            });
+        }
+
+        return res.render('uploaded', {
+            message: "Upload completed.",
+        });
+    });
+});
+
+app.get('/flist', (req, res) => {
+    const avaiableFiles = fs.readdirSync(path.join(__dirname, uploadDirectory));
+
+    res.render('flist', {
+        avaiableFiles,
+    });
+});
+
+app.listen(applicationPort, () => {
+    console.log('App listening');
+
+    allOSIps.forEach(function (ip) {
+        console.log(`${ip.name} - ${ip.address}:${applicationPort}`);
+    });
+
+    open(`http://localhost:${applicationPort}`);
+});
